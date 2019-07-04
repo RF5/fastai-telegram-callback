bbb76d57be4965a705562fded9909dad4f68cc07
Baas <matthew.baas@gmail.com>
2019-07-04 14:37:38 +0200

    add files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# fastai-telegram-callback
-A fastai training callback that messages you the metrics on telegram after each epoch
+# Fastai Telegram notifier callback
+TL;DR: 
+- A fastai training callback that notifies you after each epoch via telegram of the latest training metrics. 
+- The notification includes all the information usually printed out each epoch if you were using the console/notebooks.
+
+## Requirements
+- [fastai](https://docs.fast.ai)
+- [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot/)
+
+## Getting Started
+See the `getting_started.ipynb` notebook on how to use it in fastai's basic MNIST tutorial. Nice and short.
+
+## How to use
+- Install the requirements
+- Add the `telegram_notifier.py` somewhere in your project and import it in your training script
+- When you define your learner, add the `TelegramNotifier` to your `callback_fns`, specifying your bot token and chat id using fastai's `partial` function, or hard-code it, whatever you find more easy.
+
+Done :). 
+
+Now after each epoch you get a message with all your metrics and losses, looking something like:
+
+![something like this](http://magaimg.net/img/8dg6.png)
+
+So now whenever an epoch finishes, you will get notified with a message like this, making sure you always have the latest updates of how your training is going. Of course, if you have more metrics then they will be included in the notificaiton message.
---
+++ telegram_notifier.py
@@ -0,0 +1,40 @@
+# pip requirements:
+# fastai
+# python-telegram-bot
+
+from telegram.ext import Updater
+from fastai.train import Learner, LearnerCallback
+from fastai.metrics import MetricsList
+from fastai.core import ifnone
+from torch import Tensor
+from time import time
+from fastprogress.fastprogress import format_time
+
+class TelegramNotifier(LearnerCallback):
+    "A `LearnerCallback` that notifies you via telegram when an epoch is done, and its associated metrics."
+    def __init__(self, learn:Learner, chat_id:int=None, token:str=None): 
+        super().__init__(learn)
+        if token is None:
+            raise ValueError("Please supply your bot token")
+        if chat_id is None:
+            raise ValueError("Please supply the chat id you wish to send the notifications to")
+        self.updater = Updater(token=token)
+        self.chat_id = chat_id
+        self.bot = self.updater.bot 
+        self.add_time = True
+
+    def on_epoch_begin(self, **kwargs)->None:
+        if self.add_time: self.start_epoch = time()
+        
+    def on_epoch_end(self, epoch: int, smooth_loss: Tensor, last_metrics: MetricsList, **kwargs) -> bool:
+        "Add a line with `epoch` number, `smooth_loss` and `last_metrics`."
+        msg = ','.join(self.learn.recorder.names[:(None if self.add_time else -1)]) + '\n'
+        last_metrics = ifnone(last_metrics, [])
+        stats = [str(stat) if isinstance(stat, int) else '#na#' if stat is None else f'{stat:.6f}'
+                 for name, stat in zip(self.learn.recorder.names, [epoch, smooth_loss] + last_metrics)]
+        if self.add_time: stats.append(format_time(time() - self.start_epoch))
+        str_stats = ','.join(stats)
+        msg = msg + str_stats + '\n'
+        
+        self.bot.send_message(chat_id=self.chat_id, text=msg)
+        
